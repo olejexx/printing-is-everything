08f626e7687924574e3ebf43ebacac232d584269
olejexx <pampamm377@gmail.com>
2024-09-09 16:21:30 +0300

    Update printing.py

--- printing.py
+++ printing.py
@@ -13,4 +13,10 @@ if x>y and x<0:
   print("x как всегда первиу")
 else:
   print("эх, ну ты черт канешна, не ношол посхалку")
+z=input("Пароль для посхалки:")
+if z!=1234:
+  print("parol neverniy")
+else:
+  print("харош, угодал пасхалку еще одну")
+  print("printing is everything")
 
