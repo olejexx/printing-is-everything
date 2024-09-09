bf1defdfc4bdae1d8324195a43cb5d64986c8bcc
olejexx <pampamm377@gmail.com>
2024-09-09 16:52:42 +0300

    Update printing vetka2.py

--- printing.py
+++ printing.py
@@ -9,15 +9,8 @@ print(x//y)
 print(x**y)
 print(x%y)
 
-if x>y and x<0:
-  print("x как всегда первиу")
-else:
-  print("эх, ну ты черт канешна, не ношол посхалку")
-z=int(input("Пароль для посхалки:"))
-if z<=1234 and z>=1234:
-  print("харош, угодал пасхалку еще одну") 
-  print("printing is everything")
-else:
-  print("parol neverniy")
+z=input('Напиши самое крутое имя: ')
+if z!='oleg':
+    print("ты гей")
 
 
